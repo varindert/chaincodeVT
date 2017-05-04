2cc8775786f58e050d29e0d73fa6540445d53cea
U-ETG\vtandon <vtandon@vtandont550.etg.essextec.com>
2017-05-04 13:33:25 -0500

    one more today

--- chaincode/vtdemo_finished.go
+++ chaincode/vtdemo_finished.go
@@ -154,7 +154,7 @@ func (t *SimpleChaincode) createcompany(stub shim.ChaincodeStubInterface, args [
 	contractorids := strings.ToLower(args[4])
 	taskids := strings.ToLower(args[5])
 	
-	str := `{"companyname": "` + companyname + `", "companycontact": "` + companycontact + `", "companybudget": ` + companybudget + `", "companyid": "` + companyid + `","contractorids": "` + contractorids + `","taskids": "` + taskids + `"}`
+	str := `{"companyname": "` + companyname + `", "companycontact": "` + companycontact + `", "companybudget": "` + companybudget + `", "companyid": "` + companyid + `","contractorids": "` + contractorids + `","taskids": "` + taskids + `"}`
 
 	//fmt.Println ("company parms" + companyid + "::" + companyname + "::" + companycontact + "::"+ companybudget + "::" + contractorids +"::"+ taskids + "::"+ str)
 	
